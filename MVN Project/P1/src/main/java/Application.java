b496820dbb231c7620da6ccc9f67c726ee925429
ireshmilan <ireshmilan@gmail.com>
2020-01-19 19:37:06 +0530

    maven assignment

---
+++ MVN Project/P1/src/main/java/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+
+    public void print(){
+        System.out.println("This method is for printing in the application class");
+    }
+}
---
+++ MVN Project/P2/src/main/java/Main_Two.java
@@ -0,0 +1,10 @@
+public class Main_Two {
+    public static void main(String[] args) {
+
+        Application application=new Application();
+        application.print();
+
+
+
+    }
+}
