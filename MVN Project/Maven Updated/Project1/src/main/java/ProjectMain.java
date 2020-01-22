81ba8ff605c8e4ec8989155af1fe9877079b2178
ireshmilan <ireshmilan@gmail.com>
2020-01-22 22:14:12 +0530

    Updated Maven Project

---
+++ MVN Project/Maven Updated/Project1/src/main/java/ProjectMain.java
@@ -0,0 +1,9 @@
+public class ProjectMain {
+    public static void main(String[] args) {
+        ProjectMain main = new ProjectMain();
+        main.display();
+    }
+    public void display(){
+        System.out.println("this is Project1");
+    }
+}
