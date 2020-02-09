25319261f731d3f5d2adbf08acc955c5f58316f1
ireshmilan <ireshmilan@gmail.com>
2020-02-10 00:03:43 +0530

    Modification in Controller

--- SpringBoot/Create DB/employee-demo/src/main/java/com/iresh/employeedemo/controller/EmployeeControler.java
+++ SpringBoot/Create DB/employee-demo/src/main/java/com/iresh/employeedemo/controller/EmployeeControler.java
@@ -3,10 +3,7 @@ package com.iresh.employeedemo.controller;
 import com.iresh.employeedemo.model.Employee;
 import com.iresh.employeedemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -20,6 +17,12 @@ public class EmployeeControler {
     @RequestMapping(value = "/employees", method = RequestMethod.POST)
    public Employee save(@RequestBody Employee employee){
         return employeeService.save(employee);
+    }
+
+    @RequestMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+
+        return new Employee(1,"iresh","Ruwanwella");
     }
 
 }
