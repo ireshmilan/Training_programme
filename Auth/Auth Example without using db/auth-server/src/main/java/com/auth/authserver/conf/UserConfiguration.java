36868321fa340a28ad8aa0282064637882f912df
ireshmilan <ireshmilan.gmail.com>
2020-02-26 23:48:24 +0530

    Auth example without using databases

---
+++ Auth/Auth Example without using db/auth-server/src/main/java/com/auth/authserver/conf/UserConfiguration.java
@@ -0,0 +1,18 @@
+package com.auth.authserver.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("iresh").password(passwordEncoder.encode("ireshpass")).roles("USER","ADMIN","MANAGER").authorities("CAN_READ","CAN_WRITE","CAN_DELETE").and()
+                .withUser("abishek").password(passwordEncoder.encode("abishekpass")).roles("USER").authorities("CAN_READ","CAN_WRITE");
+    }
+}
