a8f289c6d0f5b9b42305974ae18378b6b3b3891e
ireshmilan <ireshmilan@gmail.com>
2020-03-23 13:25:03 +0530

    Updated Discovery Service

---
+++ Project/discovery-services/src/main/java/iresh/car/discovery/discoveryservices/DiscoveryServicesApplication.java
@@ -0,0 +1,15 @@
+package iresh.car.discovery.discoveryservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServicesApplication.class, args);
+    }
+
+}
